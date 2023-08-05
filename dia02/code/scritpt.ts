0bfe2b7db9576de252b35ecf7788431867ed3a09
Gustavo de Paula <gdepaula@gmail.com>
2023-08-05 16:49:36 -0300

    O enum

--- dia02/code/scritpt.ts
+++ dia02/code/scritpt.ts
@@ -1,3 +1,21 @@
 // Apple, 3, true
 let item: [string, number, boolean] = ["Apple", 3, true];
 console.log(item);
+
+// const admin = 1;
+// const ready = 2;
+// const backup = 3;
+
+enum Role {
+  admin = 1,
+  ready = 2,
+  backup = 3,
+}
+
+const user = {
+  firstName: "Gustavo",
+  age: 39,
+  role: Role.backup,
+};
+
+console.log(user);
--- dia02/prod/scritpt.js
+++ dia02/prod/scritpt.js
@@ -2,3 +2,18 @@
 // Apple, 3, true
 let item = ["Apple", 3, true];
 console.log(item);
+// const admin = 1;
+// const ready = 2;
+// const backup = 3;
+var Role;
+(function (Role) {
+    Role[Role["admin"] = 1] = "admin";
+    Role[Role["ready"] = 2] = "ready";
+    Role[Role["backup"] = 3] = "backup";
+})(Role || (Role = {}));
+const user = {
+    firstName: "Gustavo",
+    age: 39,
+    role: Role.backup,
+};
+console.log(user);
