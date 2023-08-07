a6bea8dad4e3d4623c8c3dfd698102e3c2dd6000
Gustavo de Paula <gdepaula@gmail.com>
2023-08-07 09:58:39 -0300

    Dia 3 - Exercicio 5

--- dia03/code/scritpt.ts
+++ dia03/code/scritpt.ts
@@ -99,3 +99,16 @@ const members: number[] = [
 members[0] = 12345;
 
 console.log(members);
+
+// Day 3 - Exercise 5
+
+const sequence: number[] = Array.from(Array(10).keys());
+const animals: string[] = ["pangolin", "aardvark", "echidna", "binturong"];
+const stringsAndNumbers: (string | number)[] = [1, "one", 2, "two", 3, "three"];
+const allMyArrays: (string | number)[][] = [
+  sequence,
+  animals,
+  stringsAndNumbers,
+];
+
+console.log(allMyArrays);
--- dia03/prod/scritpt.js
+++ dia03/prod/scritpt.js
@@ -63,3 +63,13 @@ const members = [
 ];
 members[0] = 12345;
 console.log(members);
+// Day 3 - Exercise 5
+const sequence = Array.from(Array(10).keys());
+const animals = ["pangolin", "aardvark", "echidna", "binturong"];
+const stringsAndNumbers = [1, "one", 2, "two", 3, "three"];
+const allMyArrays = [
+    sequence,
+    animals,
+    stringsAndNumbers,
+];
+console.log(allMyArrays);
