53826d3eefc3bb114e01407241f4904093c87521
Gustavo de Paula <gdepaula@gmail.com>
2023-08-07 09:38:08 -0300

    Union Type

--- dia03/code/scritpt.ts
+++ dia03/code/scritpt.ts
@@ -8,3 +8,19 @@ const user: Users = {
 };
 
 console.log(user);
+
+function userInput(input1: number | string, input2: number | string) {
+  let result;
+  if (typeof input1 === "number" && typeof input2 === "number") {
+    result = input1 + input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  return result;
+}
+
+const combinePrices = userInput(10, 20);
+console.log(combinePrices);
+
+const combineName = userInput("Apple", "Avocado");
+console.log(combineName);
--- dia03/prod/scritpt.js
+++ dia03/prod/scritpt.js
@@ -4,3 +4,17 @@ const user = {
     age: 39,
 };
 console.log(user);
+function userInput(input1, input2) {
+    let result;
+    if (typeof input1 === "number" && typeof input2 === "number") {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
+}
+const combinePrices = userInput(10, 20);
+console.log(combinePrices);
+const combineName = userInput("Apple", "Avocado");
+console.log(combineName);
