9dc4eeec94b598494f65dff3bdec7a3183e0925a
Gustavo de Paula <gdepaula@gmail.com>
2023-08-07 09:33:01 -0300

    Type Alias

---
+++ dia02/code/scritpt.js
@@ -0,0 +1,44 @@
+"use strict";
+// Apple, 3, true
+let item = ["Apple", 3, true];
+console.log(item);
+// const admin = 1;
+// const ready = 2;
+// const backup = 3;
+var Role;
+(function (Role) {
+    Role[Role["admin"] = 1] = "admin";
+    Role[Role["ready"] = 2] = "ready";
+    Role[Role["backup"] = 3] = "backup";
+})(Role || (Role = {}));
+const user = {
+    firstName: "Gustavo",
+    age: 39,
+    role: Role.backup,
+};
+console.log(user);
+function add(num1, num2) {
+    return num1 + num2;
+}
+console.log(add(5, 8));
+function result(price) {
+    console.log("the result is " + price);
+}
+console.log(result(add(6, 3)));
+const userNew = {
+    firstName: "Gustavo",
+    age: 39,
+    lastName: "s",
+};
+userNew.lastName = "de Paula";
+let itemInput;
+let itemName;
+itemInput = 10;
+itemInput = "apple";
+if (itemInput === "string") {
+    itemName = itemInput;
+}
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+}
+console.log(generateError("The application crashed", 500));
---
+++ dia03/code/scritpt.ts
@@ -0,0 +1,10 @@
+type Users = {
+  firstName: string;
+  age: number;
+};
+const user: Users = {
+  firstName: "Gustavo",
+  age: 39,
+};
+
+console.log(user);
