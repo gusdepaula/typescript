45e91e3b3dfac283b7f501db846fd0fb2518ff60
Gustavo de Paula <gdepaula@gmail.com>
2023-08-09 07:54:44 -0300

    Properties Public e private

--- dia05/src/app.ts
+++ dia05/src/app.ts
@@ -1,6 +1,6 @@
 class Users {
   name: string;
-  balance: number;
+  private balance: number;
 
   constructor(n: string, b: number) {
     this.name = n;
@@ -13,6 +13,7 @@ class Users {
 }
 
 const user1 = new Users("Gustavo", 10);
+// user1.balance = 500;
 user1.addMoney(100);
 const user2 = new Users("De Paula", 20);
 user2.addMoney(30);
