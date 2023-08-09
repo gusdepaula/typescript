aad7f4b066338e8f745c52f11fc354011c7da8f9
Gustavo de Paula <gdepaula@gmail.com>
2023-08-09 07:52:19 -0300

    Criando um objeto com metodo

--- dia05/src/app.ts
+++ dia05/src/app.ts
@@ -6,10 +6,16 @@ class Users {
     this.name = n;
     this.balance = b;
   }
+
+  addMoney(amount: number) {
+    this.balance = this.balance + amount;
+  }
 }
 
 const user1 = new Users("Gustavo", 10);
-const user2 = new Users("De Paula", 2);
+user1.addMoney(100);
+const user2 = new Users("De Paula", 20);
+user2.addMoney(30);
 
 console.log(user1);
 console.log(user2);
