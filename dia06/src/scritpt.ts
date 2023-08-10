dd0a5ad3594691d4f6f7165702f90edf4d650c2c
Gustavo de Paula <gdepaula@gmail.com>
2023-08-10 11:24:15 -0300

    Parametro do constructor

--- dia06/src/scritpt.ts
+++ dia06/src/scritpt.ts
@@ -1,13 +1,17 @@
 class Movies {
-  readonly id: number;
-  name: string;
+  // readonly id: number;
+  // name: string;
 
-  constructor(id: number, name: string) {
-    this.id = id;
-    this.name = name;
+  constructor(
+    public readonly id: number,
+    public name: string,
+    private _price: number
+  ) {
+    // this.id = id;
+    // this.name = name;
   }
 }
 
-let movie1 = new Movies(1, "Dejavu");
+let movie1 = new Movies(1, "Dejavu", 20);
 movie1.id = 5;
 console.log(movie1);
