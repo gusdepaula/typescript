b8420919a290c479385d011c481180dc884f5c70
Gustavo de Paula <gdepaula@gmail.com>
2023-08-10 11:39:44 -0300

    Inheritance

--- dia06/dist/scritpt.js
+++ dia06/dist/scritpt.js
@@ -1,2 +1,19 @@
 "use strict";
+class Person {
+    constructor(firstName, lastName, age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+    greet() {
+        console.log("Hi");
+    }
+}
+class Clients extends Person {
+    balance() {
+        console.log("Your balance is $100");
+    }
+}
+let client1 = new Clients("Gustavo", "De Paula", 40);
+console.log(client1);
 //# sourceMappingURL=scritpt.js.map
--- dia06/src/scritpt.ts
+++ dia06/src/scritpt.ts
@@ -16,3 +16,25 @@ let movie1 = new Movies(1, "Dejavu", 20);
 movie1.id = 5;
 console.log(movie1);
 */
+
+class Person {
+  constructor(
+    public firstName: string,
+    public lastName: string,
+    public age: number
+  ) {}
+
+  greet() {
+    console.log("Hi");
+  }
+}
+
+// Cliente do banco
+class Clients extends Person {
+  balance() {
+    console.log("Your balance is $100");
+  }
+}
+
+let client1 = new Clients("Gustavo", "De Paula", 40);
+console.log(client1);
