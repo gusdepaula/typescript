57766fa52ca0e796498b7ba2d064111a8f436ebd
Gustavo de Paula <gdepaula@gmail.com>
2023-08-11 14:55:36 -0300

    Dia 7 - Exercício 5

---
+++ dia07/dist/Day7-exercise-5.js
@@ -0,0 +1,6 @@
+"use strict";
+function layEggs(quantity, color) {
+    console.log(`You just laid ${quantity} ${color} eggs. Good job!`);
+}
+layEggs(10, "white");
+//# sourceMappingURL=Day7-exercise-5.js.map
