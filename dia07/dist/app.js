4d0590859ad25dcde4f3761bb1927c1a10069c76
Gustavo de Paula <gdepaula@gmail.com>
2023-08-11 14:36:29 -0300

    Dia 7 - Exercício 2

--- dia07/dist/app.js
+++ dia07/dist/app.js
@@ -1,9 +1,2 @@
 "use strict";
-class HotelRooms {
-}
-let room = new HotelRooms();
-room.A201 = "Gustavo";
-room.A202 = "Ana";
-room.A17 = "Marcos";
-console.log(room);
 //# sourceMappingURL=app.js.map
--- dia07/src/scritpt.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const user = {
-    firstName: "Gustavo",
-    age: 39,
-};
-console.log(user);
